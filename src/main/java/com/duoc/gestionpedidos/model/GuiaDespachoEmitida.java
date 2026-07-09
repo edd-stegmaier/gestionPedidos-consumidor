efa4579477c71872fde6a3f379ba75025e587eca
Edd113388 <ed_stegmaier@hotmail.com>
2026-07-09 01:58:42 -0400

    .

--- src/main/java/com/duoc/gestionpedidos/model/GuiaDespachoEmitida.java
+++ src/main/java/com/duoc/gestionpedidos/model/GuiaDespachoEmitida.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "guiasdespacho")
+@Table(name = "guiasdespacho_emitidas")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
